8ac1d0bb69ab1833e8091d6945328e9527462616
faqihamru <faqihamru@gmail.com>
2021-05-12 08:59:44 +0700

    add sign-in service

---
+++ services/signin/main.py
@@ -0,0 +1,28 @@
+#signin service
+from flask import Flask, jsonify, request
+import firebase_admin
+from firebase_admin import credentials
+
+#add firebase credentials to the app
+cred = credentials.Certificate("path/to/serviceAccountKey.json")
+firebase_admin.initialize_app(cred)
+db = firestore.client()
+
+app = Flask(__name__)
+
+@app.route('/', method=)
+def add_user():
+    #get json file
+    data = flask.request.json
+    nik = data["nik"]
+    password = data["password"]
+
+    #add nik and password to users collection
+    doc_ref = db.collection(u'users').document(u'nik')
+    doc_ref.set({
+        u'nik' : nik,
+        u'password' : password
+    })
+
+if __name__ == '__main__':
+    app.run(debug=True)
