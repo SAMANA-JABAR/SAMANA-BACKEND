9082f883ee78928f4820be94a15f4780bead9056
faqihamru <faqihamru@gmail.com>
2021-05-22 20:28:37 +0700

    add change password service

---
+++ services/changepass/main.py
@@ -0,0 +1,38 @@
+#change password service
+from flask import Flask, jsonify, request
+import firebase_admin
+from firebase_admin import credentials
+from google.cloud import firestore
+
+#add firebase credentials to the app
+cred = credentials.Certificate("path/to/serviceAccountKey.json")
+firebase_admin.initialize_app(cred)
+db = firestore.Client()
+
+app = Flask(__name__)
+
+@app.route('/', methods=['GET', 'POST'])
+def get_user():
+    #get request form
+    nik = request.form.get("nik")
+    current_password = request.form.get("currentpass")
+    new_password = request.form.get("newpass")
+
+    #get user data from database
+    doc_ref = db.collection(u'users').document(nik)
+    doc = doc_ref.get()
+    #update existing password
+    if doc.exists:
+        user = doc.to_dict()
+        if user["password"] == current_password:
+            doc_ref.update({
+                u'password' : new_password
+            })
+            return jsonify({"status":"update password berhasil"})
+        else:
+            return jsonify({"status":"nik dan password tidak sesuai"})
+    else:
+        return jsonify({"status":"nik tidak terdaftar"})
+
+if __name__ == '__main__':
+    app.run(debug=True)
