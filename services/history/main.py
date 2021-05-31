a2b6fea51a5155a21434ac4a140e328924e1850e
faqihamru <faqihamru@gmail.com>
2021-05-31 07:01:24 +0700

    add history service

---
+++ services/history/main.py
@@ -0,0 +1,29 @@
+#history service
+from flask import Flask, jsonify, request
+import firebase_admin
+from firebase_admin import credentials
+from google.cloud import firestore
+
+#add firebase credentials to the app
+cred = credentials.Certificate("path/to/serviceAccountKey.json")
+firebase_admin.initialize_app(cred)
+db = firestore.Client()
+
+app = Flask(__name__)
+
+@app.route('/', methods=['GET', 'POST'])
+def get_history():
+    #get request form
+    nik = request.form.get("nik")
+
+    #get user data from database
+    doc_ref = db.collection(u'users').document(nik)
+    doc = doc_ref.get()
+    user = doc.to_dict()
+
+    #return field bantuan as history of bantuan
+    history = user["bantuan"]
+    return jsonify(history)
+
+if __name__ == '__main__':
+    app.run(debug=True)
